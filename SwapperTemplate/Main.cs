701a277cf107cdb90ceb1dcd6dfe55c3d1a99879
DRQSuperior <74627000+DRQSuperior@users.noreply.github.com>
2021-05-27 01:48:08 -0500

    Add files via upload

---
+++ SwapperTemplate/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwapperTemplate
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void siticoneControlBox1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void SidePannel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
